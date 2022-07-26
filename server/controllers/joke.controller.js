b32a8bdf57390c47542b1b4055895720286d6c6d
Saptarsi Roy <saptarsiroy12@gmail.com>
2022-07-26 22:15:19 +0530

    arranged files in folders

---
+++ server/controllers/joke.controller.js
@@ -0,0 +1,35 @@
+/* 
+    Joke Controller
+*/
+
+// import joke model
+const Joke = require('../models/joke.model');
+
+// function to get all jokes
+exports.getAllJokes = async (req, res) => {
+    try {
+        const jokes = await Joke.find();
+        const count = jokes.length;
+        res.status(200).json({ jokes, count });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+// function to post a joke
+exports.postJoke = async (req, res) => {
+    const user = req.user;
+    const body = req.body;
+    try {
+        const joke = await Joke.create({
+            joke: body.joke,
+            jokedBy: user._id,
+        });
+        res.status(201).json({ message: 'Joke created successfully', joke });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+// export joke controller
+module.exports = exports;
---
+++ server/controllers/user.controller.js
@@ -0,0 +1,38 @@
+/* 
+    User Controller
+*/
+// import user model
+const User = require('../models/user.model');
+
+// function to register a user
+exports.registerUser = async (req, res) => {
+    const body = req.body;
+    try {
+        const user = await User.create(body);
+        res.status(201).json({ message: 'User created successfully', user });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+// function to login a user
+exports.loginUser = async (req, res) => {
+    const body = req.body;
+    try {
+        const user = await User.findOne({ email: body.email });
+        if (!user) {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+        const isMatch = await user.comparePassword(body.password);
+        if (!isMatch) {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+        const token = user.generateAuthToken();
+        res.status(200).json({ message: 'User logged in successfully', token });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+// export user controller
+module.exports = exports;
