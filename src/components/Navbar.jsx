ed1cf713bf0dacf540fa695b8036196cfe34429e
Saptarsi Roy <saptarsiroy12@gmail.com>
2022-07-27 22:09:28 +0530

    added navbar collapse

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,32 +6,38 @@ import { LinkContainer } from "react-router-bootstrap";
 
 export default function NavbarComponent() {
   return (
-    <Navbar bg="dark" variant="dark" sticky="top">
+    <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
       <Container>
         <Navbar.Brand>Jokes</Navbar.Brand>
-        <Nav className="me-auto">
-          <LinkContainer to="/">
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/add">
-            <Nav.Link>Add Joke</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/contact">
-            <Nav.Link>Contact</Nav.Link>
-          </LinkContainer>
-        </Nav>
-        <Nav>
-          <LinkContainer to="/login">
-            <Nav.Link>
-              <Button variant="outline-light">Login</Button>
-            </Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/register">
-            <Nav.Link>
-              <Button variant="outline-light">Register</Button>
-            </Nav.Link>
-          </LinkContainer>
-        </Nav>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          id="responsive-navbar-nav"
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/add">
+              <Nav.Link>Add Joke</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contact">
+              <Nav.Link>Contact</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <Nav>
+            <LinkContainer to="/login">
+              <Nav.Link>
+                <Button variant="outline-light">Login</Button>
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/register">
+              <Nav.Link>
+                <Button variant="outline-light">Register</Button>
+              </Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
