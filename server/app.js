1638ca1a5b5762e4099ed1bba8f8f182e1a4c5af
Saptarsi Roy <saptarsiroy12@gmail.com>
2022-07-29 13:48:56 +0530

    added option for postman with no origin in cors options

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const whitelist = [
 // cors options to allow cross origin requests
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
