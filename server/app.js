d57b54dffb57676c57c4142cff4fc91749e2d15e
Saptarsi Roy <saptarsiroy12@gmail.com>
2022-07-29 15:00:56 +0530

    forced http to https redirects

--- server/app.js
+++ server/app.js
@@ -5,10 +5,19 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const connection = require("./db.config");
 
+// force https
+const forceSSL = (req, res, next) => {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect(["https://", req.get("Host"), req.url].join(""));
+  }
+  return next();
+};
+
 // whitelist of domains that can access the API
 const whitelist = [
   "http://localhost:3000",
   "https://jokesapp3039.herokuapp.com",
+  "http://jokesapp3039.herokuapp.com",
 ];
 
 // cors options to allow cross origin requests
@@ -40,8 +49,14 @@ app.use(bodyParser.json());
 // use cors to allow cross origin resource sharing
 app.use(cors(corsOptions));
 
+// use forceSSL to force https
+app.use(forceSSL);
+
 // server react app
 app.use(express.static(path.join(__dirname, "..", "build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 // register routes
 app.use("/api", require("./routes/joke.routes"));
