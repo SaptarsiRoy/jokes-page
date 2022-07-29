6c2a996d41be8f01e1d04e609d421332c04f7a85
Saptarsi Roy <saptarsiroy12@gmail.com>
2022-07-29 13:36:30 +0530

    completed login with cookies

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,61 +1,112 @@
 // react hooks
 import { useState } from "react";
 
+// react router dom
+import { useNavigate } from "react-router-dom";
+
 // react bootstrap components
-import { Form, Button, FloatingLabel } from "react-bootstrap";
+import { Form, Button, FloatingLabel, Alert } from "react-bootstrap";
+
+// cookies
+import { useCookies } from "react-cookie";
+
+// icons
+import Eye from "../../assets/eye.svg";
+import EyeOff from "../../assets/cross_eye.svg";
 
 // import styles
 import styles from "./Login.module.css";
 
 export default function Login() {
+  // eslint-disable-next-line no-unused-vars
+  const [cookies, setCookie] = useCookies(["user"]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(email, password);
-
+    try {
+      const response = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      setCookie("user", data, { path: "/" });
+      navigate("/", { replace: true });
+    } catch (error) {
+      setShowAlert(true);
+      setError(error.message);
+    }
     // clear form
     setEmail("");
     setPassword("");
   };
   return (
-    <div style={{ display: "flex", alignItems: "center", marginTop: "20rem" }}>
-      <main className={`w-100 m-auto  ${styles["form-signin"]}`}>
-        <Form onSubmit={handleSubmit}>
-          <div className="text-center">
-            <h1 className="h3 mb-3 fw-normal" style={{ color: "white" }}>
-              Please sign in
-            </h1>
-          </div>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label visuallyHidden>Email address</Form.Label>
-            <FloatingLabel controlId="floatingEmail" label="Enter email">
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </FloatingLabel>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label visuallyHidden>Password</Form.Label>
-            <FloatingLabel controlId="floatingPassword" label="Enter password">
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </FloatingLabel>
-          </Form.Group>
-          <Button className="w-100" type="submit">
-            Login
-          </Button>
-        </Form>
-      </main>
-    </div>
+    <>
+      <Alert
+        show={showAlert}
+        dismissible
+        onClose={() => setShowAlert(false)}
+        variant="danger"
+      >
+        {error && error}
+      </Alert>
+      <div
+        style={{ display: "flex", alignItems: "center", marginTop: "20rem" }}
+      >
+        <main className={`w-100 m-auto  ${styles["form-signin"]}`}>
+          <Form onSubmit={handleSubmit}>
+            <div className="text-center">
+              <h1 className="h3 mb-3 fw-normal" style={{ color: "white" }}>
+                Please sign in
+              </h1>
+            </div>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label visuallyHidden>Email address</Form.Label>
+              <FloatingLabel controlId="floatingEmail" label="Enter email">
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label visuallyHidden>Password</Form.Label>
+              <FloatingLabel
+                controlId="floatingPassword"
+                label="Enter password"
+              >
+                <Form.Control
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <img
+                  src={showPassword ? Eye : EyeOff}
+                  alt={showPassword ? "Show password" : "Hide password"}
+                  onClick={() => setShowPassword(!showPassword)}
+                  className={styles["eye-icon"]}
+                />
+              </FloatingLabel>
+            </Form.Group>
+            <Button className="w-100" type="submit">
+              Login
+            </Button>
+          </Form>
+        </main>
+      </div>
+    </>
   );
 }
