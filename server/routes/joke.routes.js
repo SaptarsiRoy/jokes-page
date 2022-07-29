85c9e9c25306eeefa1212277ea4db8a159937523
Saptarsi Roy <saptarsiroy12@gmail.com>
2022-07-29 14:53:28 +0530

    removed auth from get random jokes

--- server/routes/joke.routes.js
+++ server/routes/joke.routes.js
@@ -2,25 +2,25 @@
     jokes routes file
 */
 // import express
-const express = require('express');
+const express = require("express");
 
 // joke controller
-const controller = require('../controllers/joke.controller');
+const controller = require("../controllers/joke.controller");
 
 // auth middleware
-const auth = require('../middleware/auth.middleware');
+const auth = require("../middleware/auth.middleware");
 
 // create a router
 const router = express.Router();
 
 // get request to get all jokes
-router.get('/jokes/:id', auth, controller.getAllJokes);
+router.get("/jokes/:id", auth, controller.getAllJokes);
 
 // get request to get a random joke
-router.get('/joke/random/:id', auth, controller.getRandomJoke);
+router.get("/joke/random", controller.getRandomJoke);
 
 // post request to add a new joke
-router.post('/jokes', auth, controller.postJoke);
+router.post("/jokes", auth, controller.postJoke);
 
 // export joke routes
 module.exports = router;
